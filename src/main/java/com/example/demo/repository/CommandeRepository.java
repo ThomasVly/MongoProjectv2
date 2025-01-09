4d33866d2bdfb663ad3586a89ab5e61b5eaff354
AdamKaoukeb <adamkaoukeb21@gmail.com>
2025-01-09 18:18:27 +0100

    tout est dans memebrecontroller

--- src/main/java/com/example/demo/repository/CommandeRepository.java
+++ src/main/java/com/example/demo/repository/CommandeRepository.java
@@ -3,7 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.object.Commande;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CommandeRepository extends MongoRepository<Commande, String> {
+
+    List<Commande> findByListe_materielContaining(String searchTerm);
 }
--- src/main/java/com/example/demo/repository/MaterielRepository.java
+++ src/main/java/com/example/demo/repository/MaterielRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MaterielRepository extends MongoRepository<Materiel, String> {
+
 }
