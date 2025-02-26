d8dd26fde4ddddd2a348279562b30613536a0f51
Antho76 <62425937+Antho76@users.noreply.github.com>
2025-02-26 11:41:46 +0100

    lien BDD avec front pour les produits + affichage seul

--- src/main/java/com/example/demo/service/MaterielService.java
+++ src/main/java/com/example/demo/service/MaterielService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class MaterielService {
     @Autowired
     private MaterielRepository materielRepository;
 
